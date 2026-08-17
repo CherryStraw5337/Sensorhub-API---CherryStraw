dee19d03bf57dc34430b50b6a894063edd990d92
Lyla Alice Estrada Pulido <lylaxtraw@gmail.com>
2026-08-17 17:53:40 -0600

    refactor: agregado lógica para US 01

--- app/models/sensor.py
+++ app/models/sensor.py
@@ -1,4 +1,5 @@
 # app/models/sensor.py
+from sqlalchemy import Boolean  # Importar Boolean explícitamente
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db import Base
@@ -7,7 +8,7 @@ from app.db import Base
 class SensorModel(Base):
     """
     Modelo de Sensor para la base de datos.
-    Cada sensor tiene un nombre, tipo, unidad de medida y un rango de valores válidos."""
+    Cada sensor tiene un nombre, tipo, unidad de medida, un rango de valores válidos y estado de actividad."""
     __tablename__ = "sensors"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
@@ -17,6 +18,9 @@ class SensorModel(Base):
     max_value: Mapped[float]
     threshold: Mapped[float | None] = mapped_column(default=None, nullable=True)  # Umbral opcional para alertas
     
-    # Usar string para evitar que mypy pida la clase antes de existir
+    # NUEVO CAMPO PARA US-01 (Soft Delete)
+    # Usamos Mapped[bool] ymapped_column con valor por defecto
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     
+    # Usar string para evitar que mypy pida la clase antes de existir
     readings = relationship("ReadingModel", back_populates="sensor")
